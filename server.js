c8e0c314cf61ca74d74d9434a49bc5eafb76616c
karan <kgalgat117@gmail.com>
2020-02-25 01:56:26 +0530

    backend setup

---
+++ server.js
@@ -0,0 +1,29 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var usersRouter = require('./routes/users');
+
+var app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.use('/users', usersRouter);
+
+app.get('*', function (req, res) {
+    res.sendFile(path.join(__dirname, 'build/index.html'))
+})
+
+app.use(function (err, req, res, next) {
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+    res.status(err.status || 500).json({ error: 'something went wrong !!!' });
+});
+
+module.exports = app;
