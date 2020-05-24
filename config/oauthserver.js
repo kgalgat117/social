b9114148b607e6037ca78dc1f589df9af8111cc5
Karan Galgat <kgalgat117@gmail.com>
2020-05-25 00:38:46 +0530

    project setup with typescript

--- config/oauthserver.js
+++
@@ -1,8 +0,0 @@
-const OAuth2Server = require('oauth2-server')
-const oAuthModel = require('./../models/auth')
-
-module.exports = new OAuth2Server({
-    model: oAuthModel,
-    accessTokenLifetime: 60 * 60,
-    allowBearerTokensInQueryString: true
-})
--- models/users.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require('mongoose')
-var DB = require('../config/db')
-
-var schema = new mongoose.Schema({
-    username: String,
-	password: String
-}, { timestamps: true })
-
-module.exports = DB.model('users', schema)
